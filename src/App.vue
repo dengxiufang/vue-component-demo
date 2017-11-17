d1556de29ad686616d463d69a0faf0c4ae8def60
dengxiufang <627223510@qq.com>
2017-11-17 12:18:07 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+      <child v-bind:message="parentMsg"></child>
+  </div>
+</template>
+
+<script>
+import child from './components/Child'
+export default {
+  name: 'app',
+  data () {
+    return {
+        parentMsg: 'hello word'
+    }
+  },
+  components: {
+      child
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
